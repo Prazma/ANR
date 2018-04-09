2ea5e56f2223bf2131970a5cd443b178ac6d7332
Tomoyoshi Yamamoto <tomoyoshi.usa@gmail.com>
2018-04-09 21:39:00 +0900

    created few sim variables
    Oh meh gawsh!
    I am way far from finiching this thing :pensive: :stuck_out_tongue:
    
    Well, I hope the pain in the ass would stop...

--- js/ai.js
+++ js/ai.js
@@ -1,15 +1,36 @@
 var ANRai = {
   startSim : function ( reactorT, dataLink ) {
     if( reactorT == "pwr" ) {
+      var safetyMode = true;
       var setup = 11;
       var time = 9.00;
       var i = setInterval(function(){
-        setup = setup - 0.01;
-        document.getElementById("cSetupInt").innerHTML = setup.split(".")[0] + ":" + setup.split(".")[1];
-        if(counter === 3) {
+        setup -= 0.01;
+        document.getElementById("cSetupInt").innerHTML =  Math.round(setup*100)/100;
+
+        //start main sim
+        if(setup <= 9.01) {
           clearInterval(i);
+          safetyMode = false;
         }
+
       }, 10);
+
+      var timeCount = 0;
+      var baseTime = 9.00;
+      var s = setInterval(function(){
+        if( safetyMode == false ) {
+          timeCount += 1;
+          //convert time count to actual time
+          var ccTime = baseTime + (timeCount/100);
+          console.log(ccConvert);
+
+          if(timeCount >= 432000) {
+            clearInterval(i);
+            alert("done with simulation");
+          }
+        }
+      }, 100);
     }
   }
 }
